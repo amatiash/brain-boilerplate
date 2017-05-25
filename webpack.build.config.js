96f00a4bd6ba7781643b1567c1851227317b411e
Anton Matiash <a.matiash@ukr.net>
2017-05-25 13:55:24 +0300

    added build

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -1,8 +1,7 @@
 var path = require('path');
 
 module.exports = {
-    watch  : true,
-    devtool: 'inline-source-map', // or eval
+    devtool: 'source-map',
     entry  : './js/main.js',
     output : {
         filename: 'main.bundle.js',
